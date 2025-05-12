8c3bc9b2f2457c7b27b5b98f126e2ffef6efcd2a
gazimaksudur2 <gazimaksudur2@gmail.com>
2025-05-12 20:21:44 +0600

    layout updated

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,7 +4,7 @@ import Footer from '../components/Shared/Footer';
 
 export default function MainLayout() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-">
       <Navbar />
       <main className="flex-grow">
         <Outlet />
